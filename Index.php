1b2c564250814d4be45f65e50af9dc30f93ea02d
AAStepanian <73479461+AAStepanian@users.noreply.github.com>
2021-05-22 18:41:48 +0200

    Update Index.php
    Udvidet form

--- Index.php
+++ Index.php
@@ -10,6 +10,10 @@
      <form method="post" action="connect.php">
      Username : <input type="text" name="username"><br><br>
      Password : <input type="password" name="password"><br><br>
+     Title : <input type="text" name="name_Title"><br><br>
+     First Name : <input type="text" name="name_First"><br><br>
+     Last Name : <input type="text" name="name_Last"><br><br>
+     User Type : <input type="text" name="user classes_classes_id"><br><br>
      <input type="submit" value="Submit">
      </form>
   </body>
