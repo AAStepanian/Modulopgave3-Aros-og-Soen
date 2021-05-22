46e4aa17fb5c6bb5ed832fa4c3eae412fff6f78c
Jenno44 <73479432+Jenno44@users.noreply.github.com>
2021-05-22 16:06:10 +0200

    Tilføjet css og generel navigation

--- header.php
+++ header.php
@@ -1,11 +1,12 @@
-<?php include("header.php"); ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/nav.css">
     <title>Modulopgave 3</title>
   </head>
-  <body style="background-color: #c67b19;">
-
+  <body>
+    <?php include("nav.php"); ?>
   </body>
 </html>
---
+++ nav.php
@@ -0,0 +1,22 @@
+<nav>
+<ul id="menu">
+  <li><a href="#">Kunder</a>
+    <ul>
+        <li><a href="#">Interne</a></li>
+        <li><a href="#">Eksterne</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Opgaver</a>
+    <ul>
+      <li><a href="#">Interne</a></li>
+      <li><a href="#">Eksterne</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Kontakter</a>
+    <ul>
+      <li><a href="#">Interne</a></li>
+      <li><a href="#">Eksterne</a></li>
+    </ul>
+  </li>
+</ul>
+</nav>
