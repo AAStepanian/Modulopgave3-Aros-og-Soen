5f877d55178c59c6028e32d28d014f4a168ef3c2
Jenno44 <73479432+Jenno44@users.noreply.github.com>
2021-05-24 15:13:19 +0200

    Oprettet ny php fil til data.
    Ny index.php skal fungere som login/forside.

--- Index.php
+++ Index.php
@@ -7,15 +7,7 @@
   </head>
   <body style="background-color: #c67b19;">
 
-     <form method="post" action="connect.php">
-     Username : <input type="text" name="username"><br><br>
-     Password : <input type="password" name="password"><br><br>
-     Title : <input type="text" name="name_Title"><br><br>
-     First Name : <input type="text" name="name_First"><br><br>
-     Last Name : <input type="text" name="name_Last"><br><br>
-     email : <input type="email" name="email"><br><br>
-     User Type : <input type="number" name="user_classes_id"><br><br>
-     <input type="submit" value="Submit">
-     </form>
+    
+
   </body>
-</html>
+  </html>
---
+++ kundedata.php
@@ -0,0 +1,47 @@
+<?php include("header.php"); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Modulopgave 3</title>
+  </head>
+  <body style="background-color: #c67b19;">
+
+     <form method="post" action="connect.php">
+     Username : <input type="text" name="username"><br><br>
+     Password : <input type="password" name="password"><br><br>
+     Title : <input type="text" name="name_Title"><br><br>
+     First Name : <input type="text" name="name_First"><br><br>
+     Last Name : <input type="text" name="name_Last"><br><br>
+     email : <input type="email" name="email"><br><br>
+     User Type : <input type="number" name="user_classes_id"><br><br>
+     <input type="submit" value="Submit">
+     </form>
+
+     <table>
+<tr>
+<th>Id</th>
+<th>Username</th>
+<th>Password</th>
+</tr>
+<?php
+$conn = mysqli_connect("localhost", "root", "root", "crmmodulopgave3");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT id, username, password FROM users";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["id"]. "</td><td>" . $row["username"] . "</td><td>"
+. $row["password"]. "</td></tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+</table>
+  </body>
+</html>
